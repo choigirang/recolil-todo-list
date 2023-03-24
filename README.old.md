1f41a99c6cf16bb18082278bb0fb495922d414c6
GiRang Choi <choigirang@kakao.com>
2023-03-24 09:55:21 +0900

    feat: 리액트 및 모듈 설치

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# recolil-todo-list
+[패스트캠퍼스] Recoil을 이용한 todo-list 만들기
---
+++ src/todoAtoms.js
@@ -0,0 +1,50 @@
+import { atom, selector } from "recoil";
+
+export const todoListState = atom({
+  key: "todoListState",
+  default: [],
+});
+
+export const todoListFilterState = atom({
+  key: "todoListFilterState",
+  default: "Show All",
+});
+
+// 셀렉터에서는 아톰의 값을 가져올 수 있다.
+// 셀렉터가 참조하는 아톰값이 바뀌게 되면 셀렉터가 업데이트 되고
+// 셀렉터가 바라보던 컴포넌트들이 리렌더링 된다.
+export const filteredTodoListState = selector({
+  key: "filteredTodoListState",
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoListState);
+
+    switch (filter) {
+      case "Show Completed":
+        return list.filter((item) => item.isComplete);
+      case "Show Uncompleted":
+        return list.filter((item) => !item.isComplete);
+
+      default:
+        return list;
+    }
+  },
+});
+
+export const todoListStatsState = selector({
+  key: "todoListStatsState",
+  get: ({ get }) => {
+    const todoList = get(todoListState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter((item) => item.isCompleted);
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum;
+
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+      percentCompleted,
+    };
+  },
+});
